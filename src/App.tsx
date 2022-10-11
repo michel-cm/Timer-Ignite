05712e5367d0a7bf9969591cf6064774d7b22be4
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-10-10 22:03:53 -0300

    types e estilos globais criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-  return <h1>app</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "sucess";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const ButtonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  sucess: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${(props) => props.theme["green-500"]};
+`;
